93cd82f8165e87d781995784f7a26d7b2cd32727
ChauNM <chau-nm@hotmail.com>
2017-09-13 16:07:12 +0700

    add actor code

---
+++ src/actor.cpp
@@ -0,0 +1,18 @@
+/*
+ * actor.cpp
+ *
+ *  Created on: Sep 13, 2017
+ *      Author: chaunm
+ */
+
+#include "actor.h"
+
+actor::actor() {
+	// TODO Auto-generated constructor stub
+
+}
+
+actor::~actor() {
+	// TODO Auto-generated destructor stub
+}
+
